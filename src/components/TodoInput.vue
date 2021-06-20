5cf106225dfe7e7a71e759736e754acdfcc087a8
es5es5 <es5e5s@nate.com>
2021-06-20 22:30:44 +0900

    Todo Component

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <label for="todo-input"></label>
+    <input type="text" name="todo-input" id="todo-input">
+    <button type="button" @click="addTodo">add</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    methods: {
+      addTodo () {
+        console.log('Add')
+      }
+    }
+  })
+</script>
+
+<style scoped>
+
+</style>
